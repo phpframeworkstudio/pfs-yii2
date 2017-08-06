b3fcc7abe52a14b8c672925893df252aff4ecda0
Dida Nurwanda <didanurwanda@gmail.com>
2017-08-06 17:25:42 +0700

    update and fix

--- assets/js/pfs.ajax.input.js
+++ assets/js/pfs.ajax.input.js
@@ -1,4 +1,4 @@
-(function (factory) {
+(function(factory) {
     "use strict";
     //noinspection JSUnresolvedVariable
     if (typeof define === 'function' && define.amd) { // jshint ignore:line
@@ -14,54 +14,37 @@
             factory(window.jQuery);
         }
     }
-}(function ($) {
+}(function($) {
     "use strict";
-
     var isEmpty, ajaxInput;
-
     isEmpty = function(value) {
         return value === null || value === undefined || value.length === 0 || $.trim(value) === '';
     }
-
     ajaxInput = function(element, options) {
         // copy
         this.options = $.extend(true, {}, $.fn.pfsAjaxInput.defaults);
-
         // merge js options
         this.options = $.extend(true, this.options, options);
-
         // merge data options
         this.options = $.extend(true, this.options, $(this).data());
-        
         // element
         this.$element = element;
         this.initialize();
     }
-
     $.extend(ajaxInput.prototype, {
-
         /**
          * Initialize plugin
          */
         initialize: function() {
             var self = this;
-
             // load depends
             if ($.isArray(self.options.depends) && self.options.depends.length > 0) {
-                this.preLoadDepends(); 
-
-            // skip: load data
+                this.preLoadDepends();
+                // skip: load data
             } else {
                 self.load();
             }
-
-            self.$element.on('change', function() {
-                self.$element.trigger('pfs:input-change');
-            });
-
-            self.$element.trigger('pfs:input-init');
         },
-
         /**
          * Pre-loading dependencies field.
          * Register all depends for watching 'change' interaction.
@@ -69,60 +52,59 @@
         preLoadDepends: function() {
             var self = this;
             self.depLength = self.options.depends.length;
-            for(var i = 0; i < self.depLength; i++) {
+            for (var i = 0; i < self.depLength; i++) {
                 var dependency = self.options.depends[i];
                 if (dependency.id) {
                     self.watcher(dependency.id, dependency.options);
                 }
             }
         },
-
         /**
          * Watcher use for detecting interaction of field.
          * @param {string} id - ID attribute of dependency element
          * @param {object} options - Configuration of depencency element
          */
         watcher: function(id, options) {
-            var self = this, $dep;
-            $("#"+ id).on('change pfs:input-change select2:select krajeeselect2:cleared', function(e) {
+            var self = this,
+                $dep;
+            $("#" + id).on('change select2:select', function(e) {
                 $dep = $(this);
                 if (!isEmpty($dep.data('select2')) && e.type === 'change') {
                     return;
                 }
                 self.depChange();
-                self.$element.find('.temporary').each(function(index, el) {
-                    $(el).remove(); 
-                });
+                self.$element.find('.temporary').remove();
             });
-
-            $("#"+ id).trigger('pfs:input-change');
+            $("#" + id).trigger('change');
         },
-
         /**
          * The action if dependencies in interaction
          */
         depChange: function() {
-            var self = this, type, value = {}, $el;
-            for(var i = 0; i < self.depLength; i++) {
+            var self = this,
+                type, value = {},
+                $el;
+            for (var i = 0; i < self.depLength; i++) {
                 var dependency = self.options.depends[i];
                 var val = null;
                 if (dependency) {
-                    $el = $("#"+ dependency.id);
+                    $el = $("#" + dependency.id);
                     val = self.getValue($el);
                 }
                 value[i] = val;
             }
             self.preDepProcess(value);
         },
-
         /**
          * Pre-pocessing for dependencies.
          * Checking rule dependency.
          * @param {array} value - Value of dependencies field.
          */
         preDepProcess: function(value) {
-            var self = this, forceDisabled = false, params = {};
-            for(var i = 0; i < self.depLength; i++) {
+            var self = this,
+                forceDisabled = false,
+                params = {};
+            for (var i = 0; i < self.depLength; i++) {
                 var options = self.options.depends[i];
                 if (options.options.required && isEmpty(value[i])) {
                     forceDisabled = true;
@@ -134,7 +116,6 @@
                 self.load(params);
             }
         },
-
         /**
          * Disable element if dependencies not complete 
          * @param {boolean} disable - Disabled or endabled the element
@@ -153,7 +134,6 @@
                 self.$element.removeAttr('readonly');
             }
         },
-
         /**
          * Request and setting data to server
          * @param {object} dataForm - Param for request data
@@ -175,7 +155,6 @@
                 });
             }
         },
-
         /**
          * Rendering elements with data from server
          * @param {object} data - Result data from server
@@ -184,7 +163,7 @@
         render: function(data, error) {
             var self = this;
             if (!error) {
-                switch(self.options.type) {
+                switch (self.options.type) {
                     case 'dropdown':
                         self.renderDropDown(data);
                         break;
@@ -198,7 +177,6 @@
                         self.renderText(data);
                         break;
                     default:
-
                         break;
                 }
             }
@@ -211,15 +189,11 @@
                     isOptGroup = true;
                 }
             });
-
             // value to array
             var values = self.splitValue(self.options.value, true);
-
             // remove temporary
             self.$element.find('.temporary').remove();
-
             var selected = false;
-
             // render dropdown with optgroup
             if (isOptGroup) {
                 $.each(data, function(label, group) {
@@ -228,30 +202,27 @@
                         'class': 'temporary'
                     });
                     $.each(group, function(val, text) {
-                        selected = values.indexOf(""+ val) > -1;
+                        selected = values.indexOf('' + val) > -1;
                         self.createOptionElement(val, text, selected)
                             .appendTo(optGroup);
                     });
                     optGroup.appendTo(self.$element);
                 });
-
-                if (self.isParents()) {
+                if (self.isParents() && selected) {
                     setTimeout(function() {
-                        self.$element.trigger(selected ? 'change' : 'pfs:input-change');
+                        self.$element.trigger('change');
                     }, 250);
                 }
-
             // render dropdown
             } else {
                 $.each(data, function(val, text) {
-                    selected = values.indexOf(""+ val) > -1;
+                    selected = values.indexOf('' + val) > -1;
                     self.createOptionElement(val, text, selected)
                         .appendTo(self.$element);
                 });
-
-                if (self.isParents()) {
+                if (self.isParents() && selected) {
                     setTimeout(function() {
-                        self.$element.trigger(selected ? 'change' : 'pfs:input-change');
+                        self.$element.trigger('change');
                     }, 250);
                 }
             }
@@ -274,52 +245,49 @@
             self.renderListInput('radio', data);
         },
         renderListInput: function(type, data) {
-            var self = this, $container, $label, $checkbox, formatter,
-                        labelOptions, itemOptions, values, checked;
-
+            var self = this,
+                $container, $label, $checkbox, formatter,
+                labelOptions, itemOptions, values, checked;
             // value to array
             values = self.splitValue(self.options.value, true);
-
             // remove temporary
             self.$element.find('.temporary').remove();
-
             // labelOptions
             labelOptions = self.options.options.labelOptions;
             if (labelOptions === undefined) {
                 labelOptions = {};
             }
-
             // itemOptions
             itemOptions = self.options.options.itemOptions;
             if (itemOptions === undefined) {
                 itemOptions = {};
             }
-
             // formatter
             formatter = self.options.options.itemClient;
-
             var index = 0;
             $.each(data, function(val, text) {
-                checked = values.indexOf(""+ val) > -1;
+                checked = values.indexOf('' + val) > -1;
                 if (formatter !== undefined) {
-                    self.$element.append(formatter(index, text, 
+                    self.$element.append(formatter(index, text,
                         self.options.options.name, checked, val));
                 } else {
-                    self.$element.append(self.createListElement(type, val, text, false, 
+                    self.$element.append(self.createListElement(type, val, text, false,
                         labelOptions, itemOptions, checked));
                 }
                 index++;
             });
-
-            if (self.isParents()) {
+            if (self.isParents() && checked) {
                 setTimeout(function() {
-                    self.$element.trigger(checked ? 'change' : 'pfs:input-change');
+                    self.$element.trigger('change');
                 }, 250);
             }
         },
         createListElement: function(type, val, text, selected, labelOptions, itemOptions, checked) {
-            var self = this, $container, $label;
-            $container = $('<div/>', {'class': type +' temporary'});
+            var self = this,
+                $container, $label;
+            $container = $('<div/>', {
+                'class': type + ' temporary'
+            });
             $label = $('<label/>', labelOptions).appendTo($container);
             $('<input/>', $.extend(true, {
                 'type': type,
@@ -327,49 +295,44 @@
                 'value': val,
                 'checked': checked
             }, itemOptions)).appendTo($label);
-            $label.append(' '+ text);
-
+            $label.append(' ' + text);
             return $container;
         },
         renderText: function(data) {
-            var self = this, values, value, sources = [], $valueEl;
-
-            $valueEl = self.$element.closest('.pfs__text-autocomplete').find('#'+ self.options.options.valueId);
-
+            var self = this,
+                values, value, sources = [],
+                $valueEl;
+            $valueEl = self.$element.closest('.pfs__text-autocomplete').find('#' + self.options.options.valueId);
             // clear on render
             self.$element.val(null);
             $valueEl.val(null);
-
             // value to array
             values = self.splitValue(self.options.value, true);
             value = (values.length ? values[0] : null);
-
             // create sources
             $.each(data, function(index, val) {
-                sources.push({value: index, label: val});
-
+                sources.push({
+                    value: index,
+                    label: val
+                });
                 // auto selected
-                if (!isEmpty(value) && (''+ index == ''+ value)) {
+                if (!isEmpty(value) && ('' + index == '' + value)) {
                     // label
                     self.$element.val(val);
-                    
                     // value
                     $valueEl.val(index);
-                    
-                    self.$element.trigger('pfs:input-change');
+                    self.$element.trigger('change');
                     delete self.options.value;
                 }
             });
-
             self.$element.autocomplete({
                 source: sources,
                 select: function(e, ui) {
                     e.preventDefault();
                     try {
                         self.$element.val(ui.item.label);
-                        $valueEl.val(ui.item.value).trigger('pfs:input-change');
-                    } catch (e) {
-                    }
+                        $valueEl.val(ui.item.value).trigger('change');
+                    } catch (e) {}
                 },
                 change: function(e, ui) {
                     e.preventDefault();
@@ -377,8 +340,8 @@
                         self.$element.val(ui.item.label);
                         $valueEl.val(ui.item.value);
                     } catch (e) {
-                        self.$element.val(null).trigger('pfs:input-change')
-                        $valueEl.val(null).trigger('pfs:input-change');
+                        self.$element.val(null).trigger('change')
+                        $valueEl.val(null).trigger('change');
                     }
                 },
                 focus: function(e, ui) {
@@ -387,17 +350,13 @@
                         self.$element.val(ui.item.label);
                         $valueEl.val(ui.item.value);
                     } catch (e) {
-                        // self.$element.val('');
-                        // $valueEl.val('')
+                        // exception
                     }
                 }
             });
-
-            self.$element.on('pfs:input-change', function(e) {
+            self.$element.on('change', function(e) {
                 if (isEmpty($(this).val())) {
-                    $valueEl.val(null).trigger('pfs:input-change');
-                } else {
-                    // console.log('apa ini', $(this).val())
+                    $valueEl.val(null).trigger('change');
                 }
             });
         },
@@ -410,7 +369,6 @@
                 var data = el.data();
                 if (data.depends && $.isPlainObject(data.depends)) {
                     $.each(data.depends, function(name, active) {
-                        // console.log(name);
                         if (name == id) {
                             isParents = true;
                         }
@@ -423,7 +381,7 @@
             var self = this;
             if (self.options.trigger == 'load') {
                 self.$element.addClass('pfs__ajax-loader');
-                switch(self.options.type) {
+                switch (self.options.type) {
                     case 'dropdown':
                         $('<option/>', {
                             text: self.options.loadingText,
@@ -431,27 +389,27 @@
                             selected: true
                         }).prependTo(self.$element);
                         self.$element.attr('disabled', true);
-                    break;
+                        break;
                     case 'text':
                         if (self.$element.prop('placeholder')) {
                             var value = self.$element.attr('placeholder');
                             self.$element.attr('data-placeholder', value);
                         }
                         self.$element.attr('placeholder', self.options.loadingText).attr('disabled', true);
-                    break;
+                        break;
                 }
             }
         },
         hideLoader: function() {
             var self = this;
             if (self.options.trigger == 'load') {
-                switch(self.options.type) {
+                switch (self.options.type) {
                     case 'dropdown':
                         self.$element.removeClass('pfs__ajax-loader');
                         self.$element.find('.pfs__loader-text').remove();
-                        self.$element.trigger('pfs:input-change');
+                        self.$element.trigger('change');
                         self.$element.removeAttr('disabled');
-                    break;
+                        break;
                     case 'text':
                         var value = self.$element.data().placeholder;
                         self.$element.removeAttr('data-placeholder');
@@ -459,9 +417,9 @@
                         if (!isEmpty(value)) {
                             self.$element.attr('placeholder', value);
                         }
-                        self.$element.trigger('pfs:input-change');
+                        self.$element.trigger('change');
                         self.$element.removeAttr('disabled');
-                    break;
+                        break;
                 }
                 self.$element.removeClass('pfs__ajax-loader');
             }
@@ -500,27 +458,24 @@
             if (isEmpty(value)) {
                 return [];
             }
-
             var values = $.trim(value).split(this.options.separator);
-
             if (trim === true) {
                 $.each(values, function(index, val) {
                     values[index] = $.trim(val);
                 });
             }
-
             return values;
         },
         getValue: function(el) {
-            var self = this, value;
-
-            switch(self.getTagName(el)) {
+            var self = this,
+                value;
+            switch (self.getTagName(el)) {
                 case 'input':
                 case 'select':
                 case 'dropdown':
                 case 'textarea':
                     value = el.val();
-                break;
+                    break;
                 case 'checkbox':
                 case 'radio':
                     var values = [];
@@ -528,12 +483,10 @@
                         values.push($(el).val());
                     });
                     value = values;
-                break;
+                    break;
                 default:
-
-                break;
+                    break;
             }
-
             if (isEmpty(value)) {
                 return null;
             }
@@ -541,7 +494,7 @@
         },
         getTagName: function(el) {
             if (el.length) {
-                switch(el[0].localName) {
+                switch (el[0].localName) {
                     case 'input':
                         return 'input';
                     case 'select':
@@ -554,19 +507,15 @@
                         } else if (el.find('input[type="radio"]').length) {
                             return 'radio';
                         }
-                    break;
+                        break;
                 }
             }
-
             return false;
         }
     });
-    
-
     $.fn.pfsAjaxInput = function(options) {
         new ajaxInput($(this), options);
     }
-
     $.fn.pfsAjaxInput.defaults = {
         url: '',
         depends: [],
